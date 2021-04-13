6161bde0ad120ced5c97099352a4dcba910717cc
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-04-12 21:01:26 -0700

    Initialized Spring Project

---
+++ springstarbucks/src/main/java/com/example/springstarbucks/SpringstarbucksApplication.java
@@ -0,0 +1,13 @@
+package com.example.springstarbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstarbucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstarbucksApplication.class, args);
+	}
+
+}
---
+++ springstarbucks/src/test/java/com/example/springstarbucks/SpringstarbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstarbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstarbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
