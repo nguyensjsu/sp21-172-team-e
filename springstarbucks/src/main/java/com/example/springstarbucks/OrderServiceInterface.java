c0d050a207e09997f4fa7e6d7e1ed78d597eea66
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-04-22 22:13:34 -0700

    Removed controllers to fix bug, will review later

--- springstarbucks/src/main/java/com/example/springstarbucks/OrderServiceInterface.java
+++ springstarbucks/src/main/java/com/example/springstarbucks/OrderServiceInterface.java
@@ -1,42 +1,42 @@
-package com.example.springstarbucks;
+// package com.example.springstarbucks;
 
-import java.util.List;
+// import java.util.List;
 
-//TODO: Check need for the imports below 
+// //TODO: Check need for the imports below 
 
-import org.springframework.web.bind.annotation.DeleteMapping; 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping; 
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus; 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.http.HttpStatus; 
-import org.springframework.lang.Nullable; 
+// import org.springframework.web.bind.annotation.DeleteMapping; 
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping; 
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.ResponseStatus; 
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.server.ResponseStatusException;
+// import org.springframework.beans.factory.annotation.Autowired; 
+// import org.springframework.http.HttpStatus; 
+// import org.springframework.lang.Nullable; 
 
-import com.example.springstarbucks.StarbucksOrder;
+// import com.example.springstarbucks.StarbucksOrder;
 
-public interface OrderServiceInterface {
+// public interface OrderServiceInterface {
 
-    //Get all orders
-        List<Orders> all(); 
+//     //Get all orders
+//         List<StarbucksOrder> all(); 
 
-    //Create new order
-        StarbucksOrder newOrder(@PathVariable String regid, @RequestBody StarbucksOrder order);
+//     //Create new order
+//         StarbucksOrder newOrder(@PathVariable String regid, @RequestBody StarbucksOrder order);
     
-    //Get order details
-        StarbucksOrder getActiveOrder(@PathVariable String regid, HttpServletResponse response);
+//     //Get order details
+//         StarbucksOrder getActiveOrder(@PathVariable String regid, HttpServletResponse response);
     
-    //Clear active order
-        String deleteActiveOrder(@PathVariable String regid);
+//     //Clear active order
+//         String deleteActiveOrder(@PathVariable String regid);
 
-    //Delete all orders
-        void deleteAll();
+//     //Delete all orders
+//         void deleteAll();
 
-    //Process order payment
-        StarbucksCard processOrder(@PathVariable String regid, @PathVariable String cardnum);
+//     //Process order payment
+//         //StarbucksCard processOrder(@PathVariable String regid, @PathVariable String cardnum);
     
-}
+// }
