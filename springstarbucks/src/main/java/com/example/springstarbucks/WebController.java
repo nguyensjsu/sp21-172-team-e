17767ccd6a09369a37e45e8a4caa106c08b069e7
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-04-22 23:42:44 -0700

    fixed duplicate routing

--- springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
+++ springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
-	@GetMapping("/")
+	@GetMapping("/home")
 	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "home";
 	}
 
-	@GetMapping("/menu")
+	@GetMapping("/menupage")
 	public String menu(String name, Model model) {
 		return "menu";
 	}
 
-	@GetMapping("/rewards")
+	@GetMapping("/rewardspage")
 	public String rewards(String name, Model model) {
 		return "rewards";
 	}
 
-	@GetMapping("/cards")
+	@GetMapping("/cardspage")
 	public String cards(String name, Model model) {
 		return "cards";
 	}
