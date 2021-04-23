7a9724606374351bab5e5fb2786ec833f1c6c22f
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-04-22 23:44:43 -0700

    Connected new routes to html

--- springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
+++ springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "home";
@@ -31,7 +31,7 @@ public class WebController {
 	}
 
 	//Goal: pass the drink value into the drink template
-	@GetMapping("/menu/drink/")
+	@GetMapping("/menupage/drink/")
 	public String drinktemplate(@RequestParam(value="drink", required=true) String drink, Model model) {
 		model.addAttribute("drink", drink);
 		return "drinktemplate";
