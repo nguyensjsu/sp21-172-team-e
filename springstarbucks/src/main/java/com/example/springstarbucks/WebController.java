256c458566cdb3795ee1300d9a8a8e9c6a1edbec
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-04-29 14:06:40 -0700

    Added passable drinks to template

--- springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
+++ springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
@@ -3,6 +3,7 @@ package com.example.springstarbucks;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,9 +32,11 @@ public class WebController {
 	}
 
 	//Goal: pass the drink value into the drink template
-	@GetMapping("/menupage/drink/")
-	public String drinktemplate(@RequestParam(value="drink", required=true) String drink, Model model) {
-		model.addAttribute("drink", drink);
+	@GetMapping("/menupage/{drinkname}")
+	public String drinktemplate(@PathVariable("drinkname") String drinkname, Model model) {
+		System.out.println("DRINK NAME: " + drinkname);
+		model.addAttribute("drinkname", drinkname);
+		System.out.println("MODEL: " + model);
 		return "drinktemplate";
 	}
 }
