ac0b75a09856f5df2cdb7ac34061731b248fb1cb
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-05-06 22:11:21 -0700

    Connected frontend to backend in cards page

--- springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
+++ springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
@@ -7,8 +7,14 @@ import com.example.springstarbucks.drinks.Drink;
 import com.example.springstarbucks.drinks.DrinkParser;
 import com.example.springstarbucks.orderapi.PaymentsCommand;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class WebController {
@@ -37,8 +44,38 @@ public class WebController {
 		return "rewards";
 	}
 
+	@PostMapping("/cardspage")
+	public String cardsPost(String name,Model model,HttpServletRequest request) {
+
+		String uri = "http://localhost:8080/cards";
+
+		
+	    RestTemplate restTemplate = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		
+		MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+		//map.add("email", "first.last@example.com");
+		
+		HttpEntity<MultiValueMap<String, String>> req = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+		
+		ResponseEntity<String> response = restTemplate.postForEntity( uri, req , String.class );
+
+		
+
+	    String cardsList = restTemplate.getForObject(uri, String.class);
+		model.addAttribute("cardsList", cardsList);
+		return "cards";
+	}
+
 	@GetMapping("/cardspage")
-	public String cards(String name, Model model) {
+	public String cardsGet(String name, Model model) {
+
+		String uri = "http://localhost:8080/cards";
+	    RestTemplate restTemplate = new RestTemplate();
+	    String cardsList = restTemplate.getForObject(uri, String.class);
+		model.addAttribute("cardsList", cardsList);
 		return "cards";
 	}
 
