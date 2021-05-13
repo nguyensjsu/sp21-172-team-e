8c6a8b77f11c2e5daab14c26104ca6e7bd5c42b4
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-05-13 15:45:38 -0700

    Added authenticated user to all other routes

--- springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
+++ springstarbucks/src/main/java/com/example/springstarbucks/WebController.java
@@ -25,6 +25,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+// MAIN CONTROLLER IMPORT
+import com.example.springstarbucks.Authentication.model.User;
+import com.example.springstarbucks.Authentication.repository.UserRepository;
+import com.example.springstarbucks.Authentication.web.dto.UserRegistrationDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 @Controller
 public class WebController {
 
@@ -97,6 +113,26 @@ public class WebController {
 
 		return "drinktemplate";
 	}
+
+	@Autowired
+    UserRepository userR;
+
+
+
+    @ModelAttribute("loggedInUser")
+    public User globalUserObject(@ModelAttribute("user") @Validated UserRegistrationDto userDto, BindingResult result,  Model model){
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+
+        if(loggedInUser instanceof AnonymousAuthenticationToken) return null;
+
+        String email = loggedInUser.getName(); 
+        User user = userR.findByEmail(email);
+        String firstname = user.getFirstName();
+        model.addAttribute("firstName", firstname);
+        model.addAttribute("emailAddress", email);
+        return user;
+
+    }
 
 }
 
--- springstarbucks/src/main/java/com/example/springstarbucks/springpayments/PaymentsController.java
+++ springstarbucks/src/main/java/com/example/springstarbucks/springpayments/PaymentsController.java
@@ -42,6 +42,22 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 
+// AUTHENTICATION IMPORT
+import com.example.springstarbucks.Authentication.model.User;
+import com.example.springstarbucks.Authentication.repository.UserRepository;
+import com.example.springstarbucks.Authentication.web.dto.UserRegistrationDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 @Slf4j
 @Controller
 @RequestMapping("/")
@@ -332,6 +348,26 @@ public class PaymentsController {
 
 
         return "payment";
+    }
+
+    @Autowired
+    UserRepository userR;
+
+
+
+    @ModelAttribute("loggedInUser")
+    public User globalUserObject(@ModelAttribute("user") @Validated UserRegistrationDto userDto, BindingResult result,  Model model){
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+
+        if(loggedInUser instanceof AnonymousAuthenticationToken) return null;
+
+        String email = loggedInUser.getName(); 
+        User user = userR.findByEmail(email);
+        String firstname = user.getFirstName();
+        model.addAttribute("firstName", firstname);
+        model.addAttribute("emailAddress", email);
+        return user;
+
     }
 
 }
